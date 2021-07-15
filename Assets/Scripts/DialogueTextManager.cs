a722d322d9a33096dda640ea32df158c5d2703e5
sizrit <sizrit@naver.com>
2021-07-16 02:00:18 +0900

    Animation 구현중

--- Assets/Scripts/DialogueTextManager.cs
+++ Assets/Scripts/DialogueTextManager.cs
@@ -37,6 +37,12 @@ public class DialogueTextManager : MonoBehaviour
     private GameObject _speaker;
     private GameObject _dialogueText;
     private Dialogue _currentDialogue;
+    private Action _dialogueTextManagerAction;
+
+    private char[] _dialogueTextData = "".ToCharArray();
+    private string _currentString = "";
+    private string _pastString = "";
+    private int _index = 0;
 
     private void OnEnable()
     {
@@ -48,12 +54,53 @@ public class DialogueTextManager : MonoBehaviour
     {
         _currentDialogue = dialogueValue;
         _speaker.GetComponent<Text>().text = _currentDialogue.speaker;
-        _dialogueText.GetComponent<Text>().text = _currentDialogue.dialogueText;
+        
+        _dialogueTextData = _currentDialogue.dialogueText.ToCharArray();
+        //_dialogueText.GetComponent<Text>().text = _currentDialogue.dialogueText;
     }
     
     private void SetDialogueEffect()
     {
-        this.gameObject.GetComponent<DialogueTextEffectManager>().SetDialogueTextEffect();
+       // this.gameObject.GetComponent<DialogueTextEffectManager>().SetDialogueTextEffect();
+    }
+
+
+    private GameObject _currnetDialogueText;
+    
+    private void func0(){}
+    
+    private float _fadeSpeed = 0.03f;
+    private void DialogueTextAnimation_FadeIn()
+    {
+        Color color = _dialogueText.GetComponent<Text>().color;
+        color.a += _fadeSpeed;
+        if (color.a > 0.95)
+        {
+            color.a = 1;
+            _dialogueTextManagerAction = new Action(func0);
+            _dialogueTextManagerAction += DialogueTextAnimation_Add;
+        }
+        _dialogueText.GetComponent<Text>().color = color;
+    }
+    
+    private void DialogueTextAnimation_Add()
+    {
+        if (_index < _dialogueTextData.Length + 1)
+        {
+            _currentString += _dialogueTextData[_index];
+        }
+        
+        if (_index > 0)
+        {
+            _pastString += _dialogueTextData[_index - 1];
+        }
+
+        _index++;
+        if (_index == _dialogueTextData.Length)
+        {
+            _dialogueTextManagerAction = new Action(func0);
+            
+        }
     }
     
     // Start is called before the first frame update
@@ -65,6 +112,6 @@ public class DialogueTextManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _dialogueTextManagerAction();
     }
 }
