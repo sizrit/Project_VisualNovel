8c710788f5b23f898484532230b4e69ef0658494
sizrit <sizrit@naver.com>
2021-07-16 01:59:36 +0900

    DialogueTextEffect 중 color 관해서 구현

--- Assets/Scripts/DialogueTextEffectManager.cs
+++ Assets/Scripts/DialogueTextEffectManager.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum DialogueTextColor
 {
-    
+    White,
+    Red,
+    Black
 }
 
 public enum DialogueTextEffect
@@ -14,19 +18,39 @@ public enum DialogueTextEffect
 
 public class DialogueTextEffectManager : MonoBehaviour
 {
-    public void SetDialogueTextEffect()
+    private Dialogue _currentDialogue;
+    private GameObject _dialogueText;
+
+    private void OnEnable()
     {
-        
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        _dialogueText = this.transform.GetChild(1).gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetDialogueTextEffect(Dialogue dialogueValue)
     {
-        
+        _currentDialogue = dialogueValue;
+        SetDialogueTextColor(_currentDialogue.color);
     }
+
+    private void SetDialogueTextColor(string colorValue)
+    {
+        Color color = Color.white;
+        switch (colorValue)
+        {
+            case "White":
+                color=Color.white;
+                break;
+            
+            case "Red":
+                color =Color.red;
+                break;
+            
+            case "Black":
+                color = Color.black;
+                break;
+        }
+
+        _dialogueText.GetComponent<Text>().color = color;
+    }
+    
 }
