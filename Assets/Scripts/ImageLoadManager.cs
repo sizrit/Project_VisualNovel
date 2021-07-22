e0763cc3170bcd1f8a6e4f9f781147f86cffb5a4
sizrit <sizrit@naver.com>
2021-07-22 14:26:30 +0900

    ImageLoadManager 구현 Resources/Images/Prefabs 안에 Prefabs 으로 이미지셋을 만들어 두면 이름을 통해 불러오는 구조

---
+++ Assets/Scripts/ImageLoadManager.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ImageLoadManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static ImageLoadManager _instance;
+
+    public static ImageLoadManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            var obj = FindObjectOfType<ImageLoadManager>();
+            if (obj != null)
+            {
+                _instance = obj;
+            }
+            else
+            {
+                GameObject gameObject = new GameObject("ImageLoadManager");
+                _instance = gameObject.AddComponent<ImageLoadManager>();
+            }
+        }
+
+        return _instance;
+    }
+
+    private void Awake()
+    {
+        var obj = FindObjectsOfType<ImageLoadManager>();
+        if (obj.Length != 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+
+    private readonly Dictionary<string, GameObject> _imagePrefabsList = new Dictionary<string, GameObject>();
+
+    private void LoadAllPrefabs()
+    {
+        GameObject[] imagePrefabs = Resources.LoadAll<GameObject>("Images/Prefabs");
+        foreach (var imagePrefab in imagePrefabs)
+        {
+            _imagePrefabsList.Add(imagePrefab.name,imagePrefab);
+            Debug.Log(imagePrefab.name);
+        }
+    }
+
+    private void OnEnable()
+    {
+        LoadAllPrefabs();
+    }
+
+    public void SetImage(string imageIdValue)
+    {
+        Instantiate(_imagePrefabsList[imageIdValue], this.transform);
+    }
+}
