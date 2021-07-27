a99d8a452a1848f3f018ec911623834126fa77a9
sizrit <sizrit@naver.com>
2021-07-27 14:55:24 +0900

    ClickSystem Inventory 클릭시 반응하도록 만듬 수정 필요

--- Assets/Scripts/Click/ClickSystem.cs
+++ Assets/Scripts/Click/ClickSystem.cs
@@ -5,7 +5,40 @@ using UnityEngine;
 
 public class ClickSystem : MonoBehaviour
 {
+    #region Singleton
 
+    private static ClickSystem _instance;
+
+    public static ClickSystem GetInstance()
+    {
+        if (_instance == null)
+        {
+            var obj = FindObjectOfType<ClickSystem>();
+            if (obj != null)
+            {
+                _instance = obj;
+            }
+            else
+            {
+                GameObject gameObject = new GameObject("GameSystem");
+                _instance = gameObject.AddComponent<ClickSystem>();
+            }
+        }
+
+        return _instance;
+    }
+
+    private void Awake()
+    {
+        var obj = FindObjectsOfType<ClickSystem>();
+        if (obj.Length != 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    #endregion
+    
     delegate void ClickDelegate();
 
     private ClickDelegate _clickDelegate;
@@ -48,6 +81,8 @@ public class ClickSystem : MonoBehaviour
                         break;
                 
                     case "Inventory":
+                        GameObject.Find("InventoryButton").transform.GetChild(0).gameObject.SetActive(true);
+                        GameSystem.GetInstance().PauseOn();
                         break;
                 }
             }
@@ -58,6 +93,15 @@ public class ClickSystem : MonoBehaviour
     {
         _clickDelegate();
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GameSystem.GetInstance().PauseOn();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GameSystem.GetInstance().PauseOff();
+        }
+        
         if (Input.GetKeyDown(KeyCode.A))
         {
             GameObject.Find("Canvas").transform.GetChild(0).gameObject.SetActive(false);
