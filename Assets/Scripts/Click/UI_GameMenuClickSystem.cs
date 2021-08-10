5aeb0d098d61b219c13870ee97f9e73392d3942a
sizrit <sizrit@naver.com>
2021-08-11 08:21:22 +0900

    MenuClickSystem -> UI_GameMenuClickSystem 이름 변경 버튼 클릭했을때 상호작용 효과들 수정

--- Assets/Scripts/Click/UI_GameMenuClickSystem.cs
+++ Assets/Scripts/Click/UI_GameMenuClickSystem.cs
@@ -9,32 +9,32 @@ public enum MenuMode
     Inventory,
 }
 
-public class MenuClickSystem : I_ClickSystem
+public class UI_GameMenuClickSystem : I_ClickSystem
 {
     #region Singleton
 
-    private static MenuClickSystem _instance;
+    private static UI_GameMenuClickSystem _instance;
 
-    public static MenuClickSystem GetInstance()
+    public static UI_GameMenuClickSystem GetInstance()
     {
         if (_instance == null)
         {
-            _instance=new MenuClickSystem();
+            _instance=new UI_GameMenuClickSystem();
         }
         return _instance;
     }
 
     #endregion
 
-    private PauseMenuManager _pauseMenuManager;
+    private UI_GameMenuManager _uiGameMenuManager;
     
     private MenuMode _currentMode = MenuMode.Inventory;
 
     public void OnEnable()
     {
-        _pauseMenuManager = PauseMenuManager.GetInstance();
+        _uiGameMenuManager = UI_GameMenuManager.GetInstance();
     }
-    
+
     public void Click()
     {
         if (Input.GetMouseButtonDown(0))
@@ -57,7 +57,7 @@ public class MenuClickSystem : I_ClickSystem
                 if (_currentMode != MenuMode.Inventory)
                 {
                     _currentMode = MenuMode.Inventory;
-                    _pauseMenuManager.SetMenuMode(MenuMode.Inventory);
+                    _uiGameMenuManager.SetMenuMode(MenuMode.Inventory);
                 }
                 break;
             
@@ -65,13 +65,14 @@ public class MenuClickSystem : I_ClickSystem
                 if (_currentMode != MenuMode.DialogueLog)
                 {
                     _currentMode = MenuMode.DialogueLog;
-                    _pauseMenuManager.SetMenuMode(MenuMode.DialogueLog);
+                    _uiGameMenuManager.SetMenuMode(MenuMode.DialogueLog);
                 }
                 break;
             
             case "Back":
                 _currentMode = MenuMode.Inventory;
-                _pauseMenuManager.RemoveAllInMain();
+                _uiGameMenuManager.RemoveAllInMain();
+                _uiGameMenuManager.RemoveGameMenu();
                 ClickSystem.GetInstance().SetClickMode(ClickMode.StoryBoard);
                 break;
         }
