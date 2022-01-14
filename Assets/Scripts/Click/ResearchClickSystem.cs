6a82cb33c85d7f973af270017c4b093d76a7125b
sizrit <sizrit@naver.com>
2022-01-14 14:55:47 +0900

    Research 모드에서 필요한 Class 아직 구현중

---
+++ Assets/Scripts/Click/ResearchClickSystem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchClickSystem : I_ClickSystem
+{
+    #region Singleton
+
+    private static ResearchClickSystem _instance;
+
+    public static ResearchClickSystem GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new ResearchClickSystem();
+        }
+        return _instance;
+    }
+
+    #endregion
+
+    private void CheckClick()
+    {
+        
+    }
+    
+    public void Click()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(ray);
+
+            foreach (var hit in hits)
+            {
+                hit.transform.GetComponent<IResearchClickable>().Click();
+            }
+        }
+    }
+}
