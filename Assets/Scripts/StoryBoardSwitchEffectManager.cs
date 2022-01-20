a56ac06d09d80c82e618fd04c720c1c13e1a3d27
sizrit <sizrit@naver.com>
2022-01-20 14:54:48 +0900

    StoryBoardSwitchEffectManager 버그 수정

--- Assets/Scripts/StoryBoardSwitchEffectManager.cs
+++ Assets/Scripts/StoryBoardSwitchEffectManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryBoardSwitchEffectManager : MonoBehaviour
 {
@@ -30,42 +31,66 @@ public class StoryBoardSwitchEffectManager : MonoBehaviour
 
     #endregion
 
+    [SerializeField] private GameObject gameObject01;
+
+    private float _speed = 0.01f;
+    
     private Action _callBack = delegate { };
     private Action _effect = delegate { };
 
     public void SwitchOnEffect(Action func)
     {
         ResetCallBack();
-        FadeSystem.GetInstance().SetCallBack(func);
-        FadeSystem.GetInstance().CallFadeSystem(FadeMode.FadeIn);
-        // _effect = SwitchOnEffectAnimation;
-        // _callBack = func;
+        //FadeSystem.GetInstance().SetCallBack(func);
+        //FadeSystem.GetInstance().CallFadeSystem(FadeMode.FadeIn);
+         _effect = SwitchOnEffectAnimation;
+         _callBack = func;
     }
 
     public void SwitchOffEffect(Action func)
     {
         ResetCallBack();
-        FadeSystem.GetInstance().SetCallBack(func);
-        FadeSystem.GetInstance().CallFadeSystem(FadeMode.FadeOut);
+        // FadeSystem.GetInstance().SetCallBack(func);
+        // FadeSystem.GetInstance().CallFadeSystem(FadeMode.FadeOut);
         
-        // _effect = SwitchOffEffectAnimation;
-        // _callBack = func;
+         _effect = SwitchOffEffectAnimation;
+         _callBack = func;
     }
 
     // effect
     private void SwitchOnEffectAnimation()
     {
-        Debug.Log("StoryBoard mode witch on");
-        _effect = delegate { };
-        _callBack();
+        Color color = gameObject01.GetComponent<Image>().color;
+        if (color.a > 0.95)
+        {
+            color.a = 1;
+            gameObject01.GetComponent<Image>().color = color;
+            _effect = delegate { };
+            _callBack();
+            return;
+        }
+
+        color.a += _speed;
+        gameObject01.GetComponent<Image>().color = color;
+        //gameObject02.GetComponent<Image>().color = color;
+        //gameObject03.GetComponent<Image>().color = color;
     }
 
     // effect
     private void SwitchOffEffectAnimation()
     {
-        Debug.Log("StoryBoard mode switch off");
-        _effect = delegate { };
-        _callBack();
+        Color color = gameObject01.GetComponent<Image>().color;
+        if (color.a < 0.05)
+        {
+            color.a = 0;
+            gameObject01.GetComponent<Image>().color = color;
+            _effect = delegate { };
+            _callBack();
+            return;
+        }
+
+        color.a -= _speed;
+        gameObject01.GetComponent<Image>().color = color;
     }
 
     private void ResetCallBack()
