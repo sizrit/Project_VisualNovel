18620b0c6a680b19ff996561449b0fa04fcebc4b
sizrit <sizrit@naver.com>
2022-01-26 14:54:05 +0900

    Editor 만들어보는중 2

--- Assets/Scripts/Editor/StoryBoardDataEditorGraphView.cs
+++ Assets/Scripts/Editor/StoryBoardDataEditorGraphView.cs
@@ -1,4 +1,6 @@
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Editor
@@ -7,15 +9,47 @@ namespace Editor
     {
         public StoryBoardDataEditorGraphView()
         {
-            //AddGridBackGround();
+            AddGridBackGround();
+            AddStyle();
+            AddManipulators();
+        }
+
+        private StoryBoardNode CreateNode(Vector2 position)
+        {
+            StoryBoardNode node = new StoryBoardNode();
+            node.Initialize(position);
+            node.Draw();
+            return node;
+        }
+        
+        private void AddStyle()
+        {
+            StyleSheet styleSheet = (StyleSheet)EditorGUIUtility.Load("SizritStyle.uss");
+            styleSheets.Add(styleSheet);
+        }
+
+        private void AddManipulators()
+        {
+            SetupZoom( ContentZoomer.DefaultMinScale,ContentZoomer.DefaultMaxScale);
+            
+            this.AddManipulator(CreateNodeContextualMenu());
+            this.AddManipulator(new ContentDragger());
+            this.AddManipulator(new ContentZoomer());
         }
 
         private void AddGridBackGround()
         {
             GridBackground gridBackground = new GridBackground();
             gridBackground.StretchToParentSize();
-        
             Insert(0,gridBackground);
+        }
+
+        private IManipulator CreateNodeContextualMenu()
+        {
+            ContextualMenuManipulator contextualMenuManipulator = new ContextualMenuManipulator(
+                menuEvent => menuEvent.menu.AppendAction("Add Node",actionEvent => AddElement(CreateNode(actionEvent.eventInfo.localMousePosition)))
+                );
+            return contextualMenuManipulator;
         }
     }
 }
