f2b3a37397b13210e761e46c431a4d126e06df91
sizrit <sizrit@naver.com>
2022-03-15 09:20:10 +0900

    SaveManager 구현 Save 를 담당함 StoryBoard 쪽 Save 는 구현 필요

---
+++ Assets/Scripts/StoryBoardEditor/SaveManager.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace StoryBoardEditor
+{
+
+    [Serializable]
+    public class NodeData
+    {
+        public string nodeId;
+        public string prevNodeId;
+        public string nextNodeId;
+        public string inputLineId;
+        public string outputLineId;
+        public string storyBoardId;
+        public float x;
+        public float y;
+    }
+
+    [Serializable]
+    public class LineData
+    {
+        public string lineId;
+        public string node01Id;
+        public string node02Id;
+    }
+
+    [Serializable]
+    public class EtcData
+    {
+        public int nodeCount;
+        public int lineCount;
+    }
+    
+    public class SaveManager
+    {
+        #region Singleton
+
+        private static SaveManager _instance;
+
+        public static SaveManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SaveManager();
+            }
+            return _instance;
+        }
+
+        #endregion
+
+        public void Save()
+        {
+            SaveNode();
+            SaveLine();
+            SaveEtc();
+        }
+
+        private void SaveNode()
+        {
+            List<NodeData> nodeDataList = new List<NodeData>();
+            foreach (var node in NodeManager.GetInstance().GetAllNode())
+            {
+                var position = node.gameObject.transform.position;
+                NodeData newNodeData = new NodeData
+                {
+                    nodeId = node.nodeId,
+                    storyBoardId = "",
+                    nextNodeId = node.nextNode?.nodeId,
+                    prevNodeId = node.prevNode?.nodeId,
+                    inputLineId = node.inputLine?.id,
+                    outputLineId = node.outputLine?.id,
+                    x = position.x,
+                    y = position.y
+                };
+                nodeDataList.Add(newNodeData);
+            }
+
+            string jsonData = JsonConvert.SerializeObject(nodeDataList, Formatting.Indented);
+            string path = Application.dataPath+"/StoryBoardEditorSave/NodeData.json";
+            File.WriteAllText(path,jsonData);
+        }
+
+        private void SaveLine()
+        {
+            List<LineData> linDataList = new List<LineData>();
+            foreach (var line in LineManager.GetInstance().GetAllLine())
+            {
+                LineData newLineData = new LineData
+                {
+                    lineId = line.id, node01Id = line.node01.nodeId, node02Id = line.node02.nodeId
+                };
+                linDataList.Add(newLineData);
+            }
+            
+            string jsonData = JsonConvert.SerializeObject(linDataList, Formatting.Indented);
+            string path = Application.dataPath+"/StoryBoardEditorSave/LineData.json";
+            File.WriteAllText(path,jsonData);
+        }
+
+        private void SaveEtc()
+        {
+            EtcData newEtcData = new EtcData
+            {
+                nodeCount = NodeManager.GetInstance().nodeIdCount, lineCount = LineManager.GetInstance().lineCount
+            };
+
+            string jsonData = JsonConvert.SerializeObject(newEtcData, Formatting.Indented);
+            string path = Application.dataPath+"/StoryBoardEditorSave/EtcData.json";
+            File.WriteAllText(path,jsonData);
+        }
+    }
+}
