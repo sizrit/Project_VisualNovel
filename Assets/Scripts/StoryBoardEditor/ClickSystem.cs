9f72ce572f02a35c538e7d3691e9e74450124907
sizrit <sizrit@naver.com>
2022-03-16 09:15:37 +0900

    ClickSystem 구동 방식 변경

--- Assets/Scripts/StoryBoardEditor/ClickSystem.cs
+++ Assets/Scripts/StoryBoardEditor/ClickSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using UnityEngine;
 
 namespace StoryBoardEditor
@@ -41,15 +42,33 @@ namespace StoryBoardEditor
         }
 
         #endregion
-        
-        private Action _checkFunc = delegate { };
+
+        readonly List<Action<RaycastHit2D[]>> _customClickList = new List<Action<RaycastHit2D[]>>();
+        private bool _isClickEnable = true;
+
+        private Action<RaycastHit2D[]> MakeClick()
+        {
+            Action<RaycastHit2D[]> click = delegate { };
+
+            if (_isClickEnable)
+            {
+                click = Click;
+            }
+
+            foreach (var customClick in _customClickList)
+            {
+                click += customClick;
+            }
+
+            return click;
+        }
 
         private ClickMode ClickPriority(RaycastHit2D[] hits)
         {
             List<(string, ClickMode)> priorityList = new List<(string, ClickMode)>
             {
                 ("StoryBoardEditor_UI", ClickMode.UI),
-                ("StoryBoardEditor_NodeInfo",ClickMode.NodeInfo),
+                ("StoryBoardEditor_NodeInfo", ClickMode.NodeInfo),
                 ("StoryBoardEditor_NodeInput", ClickMode.NodeInput),
                 ("StoryBoardEditor_NodeOutput", ClickMode.NodeOutput),
                 ("StoryBoardEditor_Node", ClickMode.Node)
@@ -77,9 +96,49 @@ namespace StoryBoardEditor
                 {
                     return hit.transform.gameObject;
                 }
+
+                if (hit.transform.CompareTag("StoryBoardEditor_NodeInput") ||
+                    hit.transform.CompareTag("StoryBoardEditor_NodeOutput"))
+                {
+                    return hit.transform.parent.gameObject;
+                }
             }
 
             return null;
+        }
+
+        private void Click(RaycastHit2D[] hits)
+        {
+            ClickMode mode = ClickPriority(hits);
+
+            switch (mode)
+            {
+                case ClickMode.UI:
+                    UI_ButtonManager.GetInstance().UI_Click(hits);
+                    //UI_EditButton.GetInstance().Click();
+                    break;
+
+                case ClickMode.NodeInfo:
+                    NodeInfoManager.GetInstance().CheckClick(hits);
+                    break;
+
+                case ClickMode.NodeInput:
+                    NodeManipulator.GetInstance().ClickNodeInput(GetNodeFromClick(hits));
+                    break;
+
+                case ClickMode.NodeOutput:
+                    NodeManipulator.GetInstance().ClickNodeOutput(GetNodeFromClick(hits));
+                    break;
+
+                case ClickMode.Node:
+                    NodeManipulator.GetInstance().LeftClickNode(GetNodeFromClick(hits));
+                    NodeInfoManager.GetInstance().EnableNodeInfo(NodeManipulator.GetInstance().GetSelectedNode());
+                    break;
+
+                case ClickMode.Null:
+                    NodeInfoManager.GetInstance().DisableNodeInfo();
+                    break;
+            }
         }
 
         private void CheckClick()
@@ -89,44 +148,42 @@ namespace StoryBoardEditor
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(ray);
 
-                ClickMode mode = ClickPriority(hits);
-
-                switch (mode)
-                {
-                    case ClickMode.UI:
-                        UI_ButtonManager.GetInstance().UI_Click(hits);
-                        //UI_EditButton.GetInstance().Click();
-                        break;
-                    
-                    case ClickMode.NodeInfo:
-                        NodeInfoManager.GetInstance().CheckClick(hits);
-                        break;
-                    
-                    case  ClickMode.NodeInput:
-                        NodeManipulator.GetInstance().ClickNodeInput(GetNodeFromClick(hits));
-                        break;
-                    
-                    case  ClickMode.NodeOutput:
-                        NodeManipulator.GetInstance().ClickNodeOutput(GetNodeFromClick(hits));
-                        break;
-                    
-                    case ClickMode.Node:
-                        NodeManipulator.GetInstance().LeftClickNode(GetNodeFromClick(hits));
-                        NodeInfoManager.GetInstance().EnableNodeInfo(NodeManipulator.GetInstance().GetSelectedNode());
-                        break;
-
-                    case ClickMode.Null:
-                        NodeInfoManager.GetInstance().DisableNodeInfo();
-                        break;
-                }
+                Action<RaycastHit2D[]> click = MakeClick();
+                click(hits);
             }
         }
 
         private void Update()
         {
             CheckClick();
-            _checkFunc();
         }
+        
+        public void SubscribeCustomClick(Action<RaycastHit2D[]> click)
+        {
+            if (!_customClickList.Contains(click))
+            {
+                _customClickList.Add(click);
+            }
+        }
+
+        public void UnsubscribeCustomClick(Action<RaycastHit2D[]> click)
+        {
+            if (_customClickList.Contains(click))
+            {
+                _customClickList.Remove(click);
+            }
+        }
+
+        public void EnableClick()
+        {
+            _isClickEnable = true;
+        }
+
+        public void DisableClick()
+        {
+            _isClickEnable = false;
+        }
+
     }
 }
 
