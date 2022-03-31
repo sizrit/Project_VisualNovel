21ca24a8be230ec9c5291c2da071ad06261aa638
sizrit <sizrit@korea.ac.kr>
2022-04-01 01:17:23 +0900

    DialogueNodeInfo 구현중

--- Assets/Scripts/StoryBoardEditor/DialogueNodeInfo.cs
+++ Assets/Scripts/StoryBoardEditor/DialogueNodeInfo.cs
@@ -1,13 +1,47 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace StoryBoardEditor
 {
     public class DialogueNodeInfo : MonoBehaviour, INodeInfo
     {
-
+        [SerializeField] private GameObject checkBox;
+        [SerializeField] private GameObject checkBoxImage;
+        [SerializeField] private Sprite checkOnImage;
+        [SerializeField] private Sprite checkOffImage;
+        
+        [SerializeField] private GameObject storyBoardIdText;
+        [SerializeField] private GameObject storyBoardIdImage;
+        [SerializeField] private GameObject storyBoardIdInputField;
+        
+        [SerializeField] private GameObject bgIdText;
+        [SerializeField] private GameObject bgIdInputField;
+        
+        [SerializeField] private GameObject imageIdText;
+        [SerializeField] private GameObject imageIdInputField;
+        
         public void SetNodeInfo(Node node)
         {
-            
+            if (node.isUseStaticStoryBoardId)
+            {
+                EnableStoryBoardIdZone();
+            }
+            else
+            {
+                DisableStoryBoardIdZone();
+            }
+        }
+
+        private void EnableStoryBoardIdZone()
+        {
+            checkBoxImage.GetComponent<Image>().sprite = checkOnImage;
+        }
+
+        private void DisableStoryBoardIdZone()
+        {
+            checkBoxImage.GetComponent<Image>().sprite = checkOffImage;
+            storyBoardIdInputField.GetComponent<TMP_InputField>().enabled = false;
         }
 
         public void Click(RaycastHit2D[] hits)
